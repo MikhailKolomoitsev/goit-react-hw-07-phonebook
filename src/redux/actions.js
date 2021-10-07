6b2b10741dc4e20d4bc1ccaaafdefcabcdc50138
mihon.testosterone <mihon.testosterone@gmail.com>
2021-10-07 13:41:15 +0300

    1

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,6 @@
 import { createAction } from "@reduxjs/toolkit";
+export const changeFilter=createAction('contacts/changeFilter')
 
 // export const getAllContactsRequest = createAction('getAllContactsRequest');
 // export const getAllContactsSuccess = createAction('getAllContactsSuccess');
 // export const getAllContactsError = createAction('getAllContactsError');
-export const changeFilter=createAction('contacts/changeFilter')
